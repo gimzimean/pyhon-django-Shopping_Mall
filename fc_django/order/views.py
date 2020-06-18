a3c7362abfc7e9441190709430dddc1120dddb3e
GZM <gimzimean@gmail.com>
2020-06-18 11:36:44 +0900

    decorator, admin per pages

--- fc_django/order/views.py
+++ fc_django/order/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
+from django.views.generic import ListView
 from .forms import RegisterForm
+from .models import Order
+#decorator
+from django.utils.decorators import method_decorator
+from fcuser.decorators import login_required
 
 
 # Create your views here.
+@method_decorator(login_required, name='dispatch')
 class OrderCreate(FormView):
     form_class = RegisterForm
     success_url ='/product/'
@@ -17,3 +23,18 @@ class OrderCreate(FormView):
             'request': self.request
         })
         return kw  #함께 만들어서 전달하기
+@method_decorator(login_required, name='dispatch')
+class OrderList(ListView):
+    # model = Order 다른 사람들의 주문들도 볼 수 있기떄문 -> query Set
+    template_name = 'order.html'
+    context_object_name = 'order_list'
+
+    def get_queryset(self, **kwargs): # 현재 로그인한 사람거만 가져옴.(오버라이딩)
+        queryset = Order.objects.filter(fcuser__email = self.request.session.get('user'))
+        return queryset
+    
+    # def dispatch(request, *args, **kwargs)
+
+    # url에 접글했을때 클래스를 호출할땐 dispatcher이 호출됨.
+
+
--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -4,12 +4,16 @@ from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
 from order.forms import RegisterForm as OrderForm
+#decorator
+from django.utils.decorators import method_decorator
+from fcuser.decorators import admin_required
 # Create your views here.
 class ProductList(ListView):
     model = Product
     template_name = 'product.html'
     context_object_name = 'product_list'
 
+@method_decorator(admin_required, name='dispatch')
 class ProductCreate(FormView):
     template_name = 'register_product.html'
     context_object_name = 'product_list'
